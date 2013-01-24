eacfa89a8790a8d9542d326a05199f166d8847dc
ARMATAV <arman.ray.tav@gmail.com>
2013-01-24 09:27:59 -0800

    Open database connection, class Stock

--- ss.rb
+++ ss.rb
@@ -1,3 +1,13 @@
 require 'sinatra'
 require 'data_mapper'
 
+DataMapper.setup(:default, 'sqlite://#{Dir.pwd}/test.db')
+
+class Stock
+
+	include DataMapper::Resource
+
+	property :symbol, String
+	property :name, String
+
+end
